1abc80ccee3d9c5790170d8df1a8bc384cbfa784
Michael <eramehmichael2000@gmail.com>
2026-03-15 10:44:19 +0100

    upload

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -39,5 +39,18 @@
                 </div>
             </div>
         </div>
+        <!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '339fea848d6f2758df4a1e5744a1267a6aee1301';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+
     </body>
 </html>
